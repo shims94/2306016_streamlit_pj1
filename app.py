6d1e84c5949618a73c87d1be6cf8f277bc429af1
shims94 <shims94@gmail.com>
2023-06-16 12:35:41 +0900

    서식 연습

--- app.py
+++ app.py
@@ -7,104 +7,20 @@ import streamlit as st  # streamlit -> import (가져오기) -> as (st 이름)
 
 # st. -> ctrl + space -> 다양한 기능(함수, 메소드)을 가지고 있다
 
-st.title("Streamlit 이용 웹페이지")
-st.header("Day 8")
-st.subheader("기능에 대해서 배우는 중")
-st.write("sdff")
+st.title("나의 파이썬 Streamlit 웹 페이지")
+st.header("수업 8일차에 만들었어요")
+st.subheader("그래도 잘 만들었죠?")
+st.write("내가 만든 streamlit 페이지, 너를 위해 구웠지")
 
 # 기능이 실행되는 순서대로 화면에서 표현
-st.video("https://www.youtube.cozm/watch?v=54vt4Nalaa8&list=PLwQjvFya6lZIasjkCXGjcLar6EvI_V6nm&index=2")  # 유튜브 링크
+st.video("https://www.youtube.com/watch?v=SaCheA6Njc4")  # 유튜브 링크
 st.image("https://cdn.pixabay.com/photo/2023/06/02/14/12/woman-8035772_1280.jpg")  # 인터넷 링크
 st.image("https://i.imgur.com/jorp5JH.png")  # 인터넷 링크
 # 여러 가지 옵션을 넣어서 세부 기능들을 차이
-st.image("img/image1.jpg")  # 파일 경로 (app.py)
-st.image(image="img/image1.jpg")  # 키워드를 사용해서...
-st.image("img/image1.jpg", use_column_width=True)  # 파일 경로 (app.py)
-st.image("img/image1.jpg", width=100)  # 파일 경로 (app.py)
-
-# 제목 마크다운
-st.write("""
-# 가장 큰 제목 (h1 - headline1 - st.title)
-## 그 다음 큰 제목 (h2 - headline2 - st.header)
-### 그것보단 작은 제목 <- 대부분 여기까지만 씀 (h3 - headline3 - st.subheader)
-#### 좀 더 작은 제목 (h4)
-##### 이건 없겠지? (h5)
-###### 이것도 있나? (h6)
-####### 이건 없어ㅋ.
-""")
-
-# 서식
-text = """
-기울임 : *별표* 또는 _언더바_ 하나씩 써주면
-
-진하게(bold) : **별표**를 또는 __언더바__ 두개씩 써주면
-
-기울임 + 진하게(bold) : ***별표***를 또는 ___언더바___ 세개씩 써주면
-
-취소선 : ~물결표~
-
-밑줄 : <u>밑줄</u>
-
-형광펜 : <mark>형광펜</mark>
-"""
-# st.write(text)
-# 태그를 허용하는 옵션
-st.markdown(text, unsafe_allow_html=True)
-
-st.write("""
-    #### 순서가 없는 리스트
-    * 별표를 여백 1칸 이상과 사용하면 순서가 없는 리스트
-    * 별표를 여백 1칸 이상과 사용하면 순서가 없는 리스트
-    * 별표를 여백 1칸 이상과 사용하면 순서가 없는 리스트
-        * 들여쓰기1
-            * 들여쓰기2
-                * 들여쓰기3
-    - (-)를 여백 1칸 이상과 사용하면 순서가 없는 리스트
-    - (-)를 여백 1칸 이상과 사용하면 순서가 없는 리스트
-    - (-)를 여백 1칸 이상과 사용하면 순서가 없는 리스트
-        - 들여쓰기1
-            - 들여쓰기2
-                - 들여쓰기3
-""")
-
-# 링크
-st.divider()
-st.subheader("링크")
-l1 = "https://naver.com"
-l2 = "https://cdn.pixabay.com/photo/2023/04/08/18/01/flower-7909902_640.jpg"
-st.write(f"""
-    * [표시할 텍스트](https://naver.com)
-    * [표시할 텍스트]({l1})
-    * ![이미지에 대한 설명](https://cdn.pixabay.com/photo/2023/04/08/18/01/flower-7909902_640.jpg)
-    * ![이미지에 대한 설명]({l2})
-    * [![이미지에 대한 설명](https://cdn.pixabay.com/photo/2023/04/08/18/01/flower-7909902_640.jpg)](https://naver.com)
-""")
-
-# 인용
-st.divider()
-st.subheader("인용")
-st.write(f"""
-    > 무언가 멋진 말 - 유명한 사람
-
-
-    > 진입장벽은 수익이다 - 어느 코딩 강사
-
-    책이나, 사람말 인용할 때...
-    > 인용 첫줄
-    > > 인용문 안에 인용임
-
-    #### 코드
-    `코드를 나타낼 때 주로 쓰이는 묶음 표시 (한줄)`
-    ```
-    여러 줄로 코드를 나타내고
-    줄바꿈도 반영하고 싶으면...
-    print("파이썬!")
-    ```
-    ```python
-    print("파이썬!")
-    ```
-""")
-
-
+st.image("img/img.png")  # 파일 경로 (app.py)
+st.image(image="img/img.png")  # 키워드를 사용해서...
+st.image("img/img.png", use_column_width=True)  # 파일 경로 (app.py)
+st.image("img/img.png", width=100)  # 파일 경로 (app.py)
 # https://imgur.com/
+
 # streamlit run app.py
--- pages/컴포넌트.py
+++ pages/컴포넌트.py
@@ -11,8 +11,69 @@ cols = st.columns(3)
 cols[0].write("🐦")
 cols[1].write("🐦")
 cols[-1].write("🐦")
-cols = cols[0].columns(3)
-# 열의 열인 거임
+cols = cols[0].columns(3) # 열의 열인 거임
 cols[0].write("🐦")
 cols[1].write("🐦")
 cols[-1].write("🐦")
+col1, col2 = st.columns(2) # 리스트 언패킹
+col1.write("왼쪽 열")
+col2.write("오른쪽 열")
+
+with col1: # col1을 기준으로 streamlit을 써주겠다
+    # 블록 (:) 을 열면 -> 이 안에서는 streamlit 기능 실행시 col1에 종속
+    st.write("왼쪽")
+with col2: # col2을 기준으로 streamlit을 써주겠다
+    # 블록 (:) 을 열면 -> 이 안에서는 streamlit 기능 실행시 col2에 종속
+    st.write("오른쪽")
+
+# tabs = st.tabs(["김치찌개", "된장찌개", "로제마라어묵찌개"])
+tab_menus = ["김치찌개", "된장찌개", "로제마라어묵찌개"]
+tab1, tab2, tab3 = st.tabs(tab_menus)
+img1 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4MFBhxEu7tdtEmSpJ-DEzVl9si8NYOiYmbruRyOr7vYS8ZMLSpu60YsPo5WtmB5xg_F0&usqp=CAU"
+tab1.image(img1)
+with tab2:
+    img2 = "https://imagescdn.gettyimagesbank.com/500/201708/jv10940386.jpg"
+    st.image(img2)
+tab3.write("이런 건 없어요... 상상도 마라요")
+
+exp = st.expander("Surprise!!!", expanded=False)
+exp.image("https://i.namu.wiki/i/5lWwYGj-VC8ZqJxug7Exm5-7rHE97fdZui3DWEAjm0zdLiBCbcdw4mLyGhcbZ_KecZOQr4rtwNJSFs63Rsdd_Q.webp")
+# with exp: ...
+
+# 입력
+st.title("입력")
+name = st.text_input("나의 이름은")  # 변수로 받을 수 있음
+name2 = st.text_input("너의 이름은")  # 변수로 받을 수 있음
+# st.text_input("")
+# st.write(name)
+# st.write(name2)
+st.write(f"신랑 {name}과 신부 {name2}는...")
+# number = st.number_input("당신의 나이는?")
+age = st.number_input("당신의 나이는?", step=1)
+st.write(f"나의 나이는 {age}세")
+height = st.number_input("당신의 키는?", step=0.1)
+st.write(f"나의 키는 {height}cm")
+# https://docs.streamlit.io/library/api-reference/widgets
+
+st.divider()
+mode = st.checkbox("강사님 잔소리모드")  # bool (T/F)
+col1, col2, col3 = st.columns(3)
+r = col1.radio("잔소리 내용 선택", ["취업", "코딩", "지각"])
+s = col2.slider("잔소리 강도 선택", min_value=1, max_value=10)
+b = col3.selectbox("잔소리 말투 선택", ["친절하게", "반말", "모욕적"])
+if mode:
+    # r -> 취업, 코딩, 지각
+    format = None
+    if b == "친절하게":
+        format = lambda x: f"여러분~ {x}"
+    elif b == "반말":
+        format = lambda x: f"야! {x}"
+    elif b == "모욕적":
+        format = lambda x: f"XXXXXX! {x}"
+    if r == "취업":
+        for i in range(s):
+            st.write(format("8월에는 자소서 넣어야겠죠?"))
+    elif r == "코딩":
+        st.write(format("저보다 파이썬 잘해요?"))
+    elif r == "지각":
+        st.write(format("9시랑 9시 1분은 다른 거에요."))
