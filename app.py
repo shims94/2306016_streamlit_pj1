4a5ffc0bd0824561c31868f8c2ab8c86390638d3
shims94 <shims94@gmail.com>
2023-06-16 10:28:00 +0900

    이미지추가

--- app.py
+++ app.py
@@ -17,10 +17,10 @@ st.video("https://www.youtube.com/watch?v=SaCheA6Njc4")  # 유튜브 링크
 st.image("https://cdn.pixabay.com/photo/2023/06/02/14/12/woman-8035772_1280.jpg")  # 인터넷 링크
 st.image("https://i.imgur.com/jorp5JH.png")  # 인터넷 링크
 # 여러 가지 옵션을 넣어서 세부 기능들을 차이
-# st.image("img/img.png")  # 파일 경로 (app.py)
-# st.image(image="img/img.png")  # 키워드를 사용해서...
-# st.image("img/img.png", use_column_width=True)  # 파일 경로 (app.py)
-# st.image("img/img.png", width=100)  # 파일 경로 (app.py)
+st.image("img/image1.jpg")  # 파일 경로 (app.py)
+st.image(image="img/image1.jpg")  # 키워드를 사용해서...
+st.image("img/image1.jpg", use_column_width=True)  # 파일 경로 (app.py)
+st.image("img/image1.jpg", width=100)  # 파일 경로 (app.py)
 # https://imgur.com/
 
 # streamlit run app.py
